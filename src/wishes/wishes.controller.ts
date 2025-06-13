7b014c59251860c9ecdb5cf492296da1a10c9b45
dmitry ragozin <dmtryragozin@gmail.com>
2025-06-13 21:56:11 +0300

    feat: /wishes/last, /wishes/top

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { WishesService } from './wishes.service';
 
 @Controller('wishes')
-export class WishesController {}
+export class WishesController {
+  constructor(private readonly wishesService: WishesService) {}
+
+  @Get('last')
+  findLast() {
+    return this.wishesService.findLast();
+  }
+
+  @Get('top')
+  findTop() {
+    return this.wishesService.findTop();
+  }
+}
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Wish } from './entities/wish.entity';
 
 @Injectable()
-export class WishesService {}
+export class WishesService {
+  constructor(
+    @InjectRepository(Wish)
+    private wishesRepo: Repository<Wish>,
+  ) {}
+
+  async findLast(): Promise<Wish[]> {
+    return this.wishesRepo.find({
+      order: { createdAt: 'DESC' },
+      take: 40,
+      relations: ['owner'],
+    });
+  }
+
+  async findTop(): Promise<Wish[]> {
+    return this.wishesRepo.find({
+      order: { copied: 'DESC' },
+      take: 20,
+      relations: ['owner'],
+    });
+  }
+}
