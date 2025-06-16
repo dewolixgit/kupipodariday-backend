3904557316f746ca1a537f474bfa47c55ea77dac
dmitry ragozin <dmtryragozin@gmail.com>
2025-06-16 22:05:57 +0300

    refactor: /wishes/last, /wishes/top

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,17 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { WishesService } from './wishes.service';
+import { WishPublicDto } from './dto/wish-public.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('wishes')
 export class WishesController {
   constructor(private readonly wishesService: WishesService) {}
 
   @Get('last')
-  findLast() {
-    return this.wishesService.findLast();
+  async findLast(): Promise<WishPublicDto[]> {
+    const wishes = await this.wishesService.findLast();
+    return plainToInstance(WishPublicDto, wishes, {
+      excludeExtraneousValues: true,
+    });
   }
 
   @Get('top')
-  findTop() {
-    return this.wishesService.findTop();
+  async findTop(): Promise<WishPublicDto[]> {
+    const wishes = await this.wishesService.findTop();
+    return plainToInstance(WishPublicDto, wishes, {
+      excludeExtraneousValues: true,
+    });
   }
 }
